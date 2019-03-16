536d9c2de33e50e823f828fe0e6518f1410405da
dtutila <daniel.tutila@gmail.com>
2019-03-16 12:45:31 -0600

    adding uniswap contracts

--- contracts/providers/UniswapSwappingProvider.sol
+++ contracts/providers/UniswapSwappingProvider.sol
@@ -17,83 +17,133 @@ contract UniswapSwappingProvider is ISwappingProvider {
 
 
 
+    event PaymentSent(
+        address indexed thisContract,
+        address merchant,
+        address customer,
+        address sourceToken,
+        address targetToken,
+        uint256 amount,
+        uint256 provider
+    );
 
-
-    constructor(address _stablePay, address _factory)
-        public ISwappingProvider(_stablePay)
-    {
-        uniswapFactory = _factory;
-
+    constructor(address _stablePay, address _exchange)
+        public ISwappingProvider(_stablePay){
+        uniswapFactory = _exchange;
     }
 
-
-
-    function swapToken(StablePayCommon.Order memory  _order)
-    public isStablePay(msg.sender)
-    returns (bool)
+    function tokenToEth(address exchange, address token, uint256 amount)
+    internal
+    returns (uint256)
     {
+        // TokenA (ERC20) to ETH conversion
+        uint inputAmountA = amount;
+        uint inputReserveA = ERC20(token).balanceOf(exchange);
+        uint outputReserveA = exchange.balance;
 
+        uint numeratorA = inputAmountA * outputReserveA * 997;
+        uint denominatorA = inputReserveA * 1000 + inputAmountA * 997;
+        uint outputAmountA = numeratorA / denominatorA;
+        return outputAmountA;
+    }
+    function ethToToken(address exchange, address token, uint256 amount)
+    internal
+    returns (uint256)
+    {
+       // ETH to TokenB conversion 
+        uint inputAmountB = amount;    
+        uint inputReserveB = exchange.balance;
+        uint outputReserveB = ERC20(token).balanceOf(exchange);
+
+        uint numeratorB = inputAmountB * outputReserveB * 997;
+        uint denominatorB = inputReserveB * 1000 + inputAmountB * 997;
+        uint outputAmountB = numeratorB / denominatorB;  
+        return outputAmountB;
+    }
+
+    function buyTokenWithETH(address exchange, address token, uint256 userInputEthValue)
+    internal
+    returns (uint256)
+    {
+       // Buy ERC20 with ETH
+        uint outputAmount = userInputEthValue;
+        uint inputReserve = exchange.balance;
+        uint outputReserve = ERC20(token).balanceOf(exchange);
+
+
+        // Cost
+        uint numerator = outputAmount * inputReserve * 1000;
+        uint denominator = (outputReserve - outputAmount) * 997;
+        uint inputAmount = numerator / denominator + 1;
+
+        return inputAmount;
+    }
+
+    function swapToken(StablePayCommon.Order _order)
+        public isStablePay(msg.sender)
+        returns (bool)
+    {
+       
         UniswapFactoryInterface uFactory = UniswapFactoryInterface(uniswapFactory);
-        UniswapExchangeInterface sourceExchange = UniswapExchangeInterface(uFactory.getExchange(_order.sourceToken));
-        UniswapExchangeInterface targetExchange = UniswapExchangeInterface(uFactory.getExchange(_order.targetToken));
+ 
 
-        require(address(sourceExchange) != 0x0, "Exchange not found for source token");
-        require(address(targetExchange) != 0x0, "Exchange not found for target token");
+        require(uFactory.getExchange(_order.sourceToken) != 0x0, "Exchange not found for source token");
+        require(uFactory.getExchange(_order.targetToken) != 0x0, "Exchange not found for target token");
+        require(ERC20(_order.sourceToken).allowance(msg.sender, address(this)) >= _order.sourceAmount, "Not enough allowed tokens.");
 
+        // move tokens to provider address
+        require(ERC20(_order.sourceToken).transferFrom(msg.sender, address(this), _order.sourceAmount), "TransferFrom invocation was not successful.");
+        
+        UniswapExchangeInterface uExchange = UniswapExchangeInterface(uFactory.getExchange(_order.sourceToken));
 
-        require(_order.targetAmount > 0 , "Target amount cannot be zero");
+        // TokenA (ERC20) to ETH conversion
+        uint256 outputAmountA = tokenToEth(uFactory.getExchange(_order.sourceToken), ERC20(_order.sourceToken), _order.sourceAmount);
 
+        // ETH to TokenB conversion 
+        uint256 outputAmountB = ethToToken(uFactory.getExchange(_order.targetToken), ERC20(_order.targetToken), outputAmountA);
 
-        //uint256 thisSourceInitialTokenBalance = ERC20(_order.sourceToken).balanceOf(address(this));
-        require(ERC20(_order.sourceToken).balanceOf(address(this)) >= _order.sourceAmount, "Not enough tokens in balance.");
-
-        uint256 ethToBuyTargetToken = targetExchange.getEthToTokenOutputPrice(_order.targetAmount);
-        uint256 sourceTokensToSell = sourceExchange.getTokenToEthOutputPrice(ethToBuyTargetToken);
-
-        // Mitigate ERC20 Approve front-running attack, by initially setting allowance to 0
-        require(ERC20(_order.sourceToken).approve(address(sourceExchange), 0), "Error mitigating front-running attack.");
-        // Set the spender's token allowance to tokenQty
-        require(ERC20(_order.sourceToken).approve(address(sourceExchange), sourceTokensToSell), "Error approving tokens for exchange."); // Set max amount.
-
-
-        uint256 tokens_sold = sourceExchange.tokenToTokenSwapOutput(
-            _order.targetAmount ,
-            sourceTokensToSell,
-
-            ethToBuyTargetToken,
-            (block.timestamp) + 300,
+        //TODO review swap with transfer
+        uint tokens_bought = uExchange.tokenToTokenSwapInput(
+            _order.sourceAmount,
+            outputAmountB,
+            outputAmountA,
+            (block.number * 900) + 300,
             _order.targetToken
         );
-
-        require(ERC20(_order.targetToken).transfer(msg.sender,  _order.targetAmount), "Source transfer invocation was not successful.");
+        
+        // Send the swapped tokens to the destination address
+        bool transferResult = ERC20(_order.targetToken).transfer(msg.sender, tokens_bought);
+        require(transferResult, "Transfer invocation was not successful.");
 
         return true;
     }
-    function swapEther(StablePayCommon.Order memory  _order)
-    public isStablePay(msg.sender)
-    payable
-    returns (bool)
+    function swapEther(StablePayCommon.Order _order)
+        public isStablePay(msg.sender)
+        payable
+        returns (bool)
     {
 
 
         UniswapFactoryInterface uFactory = UniswapFactoryInterface(uniswapFactory);
-
+ 
         require(uFactory.getExchange(_order.targetToken) != 0x0, "Exchange not found for target token");
 
-        UniswapExchangeInterface targetExchange = UniswapExchangeInterface(uFactory.getExchange(_order.targetToken));
-        uint256 ethToBuyTargetToken = targetExchange.getEthToTokenOutputPrice(_order.targetAmount);
-        require(msg.value >= ethToBuyTargetToken, "Not enough value");
-        //require(address(this).balance >= ethToBuyTargetToken, "Not enough balance");
+        UniswapExchangeInterface uExchange = UniswapExchangeInterface(uFactory.getExchange(_order.sourceToken));
 
-        uint eth_sold = targetExchange.ethToTokenSwapOutput.value(ethToBuyTargetToken)(
-            _order.targetAmount,
-            block.timestamp + 300
+        uint256 amountTokens = buyTokenWithETH(uFactory.getExchange(_order.targetToken), ERC20(_order.targetToken), msg.value);
+
+        
+        uint tokens_bought = uExchange.ethToTokenSwapInput(
+            amountTokens,        
+            (block.number * 900) + 300
         );
+        
+        // Send the swapped tokens to the destination address
+        bool transferResult = ERC20(_order.targetToken).transfer(msg.sender, tokens_bought);
+        require(transferResult, "Transfer invocation was not successful.");
 
-
-        require(ERC20(_order.targetToken).transfer(msg.sender,  _order.targetAmount), "Source transfer invocation was not successful.");
         return true;
-
+        
     }
 
     function getExpectedRate(ERC20 _sourceToken, ERC20 _targetToken, uint _sourceAmount)
@@ -102,13 +152,13 @@ contract UniswapSwappingProvider is ISwappingProvider {
     returns (bool, uint, uint)
     {
         UniswapFactoryInterface uFactory = UniswapFactoryInterface(uniswapFactory);
-        UniswapExchangeInterface sourceExchange = UniswapExchangeInterface(uFactory.getExchange(_sourceToken));
-        UniswapExchangeInterface targetExchange = UniswapExchangeInterface(uFactory.getExchange(_targetToken));
+         // TokenA (ERC20) to ETH conversion
+        uint256 outputAmountA = tokenToEth(uFactory.getExchange(_sourceToken), ERC20(_sourceToken), _sourceAmount);
 
-        uint256 ethToBuyTargetToken = targetExchange.getEthToTokenOutputPrice(_sourceAmount);
-        uint256 sourceTokensTosell = sourceExchange.getTokenToEthOutputPrice(ethToBuyTargetToken);
+        // ETH to TokenB conversion 
+        uint256 outputAmountB = ethToToken(uFactory.getExchange(_targetToken), ERC20(_targetToken), outputAmountA);
 
-        return (true, ethToBuyTargetToken, sourceTokensTosell);
+        return (true, outputAmountB, outputAmountA);
     }
 
 }
