873992bc07662596ca8a309d978cbcc1d911cc97
dtutila <daniel.tutila@gmail.com>
2019-03-24 18:17:42 -0600

    adding uniswap delpoy config

---
+++ config/infuraRopsten/uniswap.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    contracts: {
+        factory: "0xad275fbfd25bf1ba12fcf8fb8d4179c1231bfb67",
+        exchangeTemplate: "0x04dc14345711ec5fd66010c286a939d252b0ac70"
+    }
+};
