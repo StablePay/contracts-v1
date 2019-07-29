392977184db7c9690e064d6406e665b8dcc0dc5a
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2019-07-28 18:09:38 -0600

    Loging conf.

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,5 @@ const AppConfig = require('./AppConfig');
 const appConfig = new AppConfig();
 appConfig.validate();
 console.log('App configuration was validated.');
+console.log(appConfig);
 module.exports = appConfig;
