5ff985a0902f8554922f46c080c1ea6bd0572781
dmolina79 <doug.molinam@gmail.com>
2020-03-29 17:03:05 -0600

    add unit tests for modifiers

--- test/providers/DexAgSwappingProviderTest.js
+++ test/providers/DexAgSwappingProviderTest.js
@@ -3,31 +3,106 @@ const {
     ETH_ADDRESS,
 } = require('../util/consts');
 const withData = require('leche').withData;
+const KyberOrderFactory = require('../factories/KyberOrderFactory');
 
 // Mock Smart Contracts 
 const Mock = artifacts.require("./mock/Mock.sol");
-//const DexAgSwappingProviderMock = artifacts.require("./mock/provider/DexAgSwappingProviderMock.sol");
 const SimpleToken = artifacts.require("./mock/token/SimpleToken.sol");
-//const StandardTokenMock = artifacts.require("./mock/erc20/StandardTokenMock.sol");
+
+// Smart Contracts
+const DexAgSwappingProvider = artifacts.require("../../contracts/providers/DexAgSwappingProvider.sol");
 
 contract('DexAgSwappingProviderTest', accounts => {
     const owner = accounts[0];
-
+    const account1 = accounts[1];
+    const account2 = accounts[2];
+    let swappingProvider;
+    let dexAgProxyMock;
+    let stablePayMock;
     let token;
+
     beforeEach('Setup', async () => {
-        token = await SimpleToken.new({from: owner});
+        stablePayMock = await Mock.new();
+        dexAgProxyMock = await Mock.new();
+        swappingProvider  = await DexAgSwappingProvider.new(
+            stablePayMock.address,
+            dexAgProxyMock.address,
+        );
     });
 
-    withData({
-        _1_min_0_max_0_notSupported: ['0', '0', false],
-        _2_min_1_max_0_notSupported: ['1', '0', false],
-        _3_min_0_max_1_notSupported: ['0', '1', false],
-        _4_min_1_max_1_supported: ['1', '1', true],
-        _5_min_12_max_2_supported: ['12', '2', true],
-    }, function(minRate, maxRate, isSupportedRateExpected) {
-        it(t('anUser', '_isSupportedRate', 'Should be able to test whether rate is supported or not.', false), async function() {
-            //Setup
-            
+    describe('#hasValidCallData modifier', () => {
+        it(t('anUser', 'swapToken', 'Should fail if order does not have bytes calldata', true), async () => {
+            // Setup
+            const customerAddress = account1;
+            const merchantAddress = account2;
+            const sourceAmount = 100;
+            const targetAmount = 100;
+            token = await SimpleToken.new({from: customerAddress});
+            token.transfer(swappingProvider.address, 100, { from: customerAddress });
+
+            const orderArray = new KyberOrderFactory({
+                sourceToken: token.address,
+                targetToken: token.address,
+                sourceAmount: sourceAmount,
+                targetAmount: targetAmount,
+                minRate: sourceAmount,
+                maxRate: targetAmount,
+                merchantAddress: merchantAddress,
+                customerAddress: customerAddress
+            }).createOrder();
+    
+            try {
+                // Invocation
+                const result = await swappingProvider.swapToken(orderArray, {from: stablePayMock.address});
+
+                console.log('result', result);
+    
+                // Assertions
+                fail(true, "It should have failed because order does not have valid call data.")
+            } catch (error) {
+                console.error(error)
+                // Assertions
+                assert(error);
+                assert.equal(error.reason, "CallData is empty.", "Error should be because empty call data");
+            }
+        });
+    
+        it(t('anUser', 'swapEther', 'Should fail if order does not have bytes calldata', true), async () => {
+            // Setup
+            const customerAddress = account1;
+            const merchantAddress = account2;
+            const sourceAmount = 100;
+            const targetAmount = 100;
+            token = await SimpleToken.new({from: customerAddress});
+            token.transfer(swappingProvider.address, 100, { from: customerAddress });
+
+            const orderArray = new KyberOrderFactory({
+                sourceToken: token.address,
+                targetToken: token.address,
+                sourceAmount: sourceAmount,
+                targetAmount: targetAmount,
+                minRate: sourceAmount,
+                maxRate: targetAmount,
+                merchantAddress: merchantAddress,
+                customerAddress: customerAddress
+            }).createOrder();
+    
+            try {
+                // Invocation
+                const result = await swappingProvider.swapEther(orderArray, {from: stablePayMock.address, value: 100 });
+
+                console.log('result', result);
+    
+                // Assertions
+                fail(true, "It should have failed because order does not have valid call data.")
+            } catch (error) {
+                console.error(error)
+                // Assertions
+                assert(error);
+                assert.equal(error.reason, "CallData is empty.", "Error should be because empty call data");
+            }
         });
     });
+
+  
 });
