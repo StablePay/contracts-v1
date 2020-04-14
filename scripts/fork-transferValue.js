bfb6226d64820d7fe94ec931ec215375d68749a8
dmolina79 <doug.molinam@gmail.com>
2020-04-14 12:40:28 -0600

    fix scripts and contract addresses in ganache

--- scripts/fork-transferValue.js
+++ scripts/fork-transferValue.js
@@ -1,14 +1,14 @@
 /**
     Purpose:
     It gets the expected rate for a specific swapping provider.
-​
+
     How do I execute this script?
-​
+
     truffle exec ./scripts/getExpectedRate.js --network infuraRopsten
  */
 // Smart contracts
 // const ERC20 = artifacts.require("./services/erc20/ERC20.sol");
-​
+
 // Util classes
 const BigNumber = require('bignumber.js');
 // const {
@@ -18,8 +18,9 @@ const BigNumber = require('bignumber.js');
 // } = require('../utils/tokens');
 const assert = require('assert');
 const ProcessArgs = require('../src/utils/ProcessArgs');
+
 const processArgs = new ProcessArgs();
-​
+
 /**
     Script Arguments
  */
@@ -32,44 +33,44 @@ const unlockedAccount = '0x742d35cc6634c0532925a3b844bc454e4438f44e';
 const DAI_WHALE = '0xe092436d60dd92170326b7782ea8d393cf67941d';
 const receiverIndex = 0;
 const amount = '10'; // WETH
-​
+
 module.exports = async (callback) => {
-    try {
-        const network = processArgs.network();
-        console.log(`Script will be executed in network ${network}.`)
-​
-        const accounts = await web3.eth.getAccounts();
-        assert(accounts, 'Accounts is undefined.');
-​
-        const receiver = accounts[receiverIndex];
-        assert(receiver, 'Receiver is undefined.');
-​
-        const amountWei = BigNumber(amount.toString()).times(10 ** 18);
-        //await receiver.transfer({ from: unlockedAccount, value: amountWei });
-        await web3.eth.sendTransaction({from: unlockedAccount, to:receiver, value: amountWei});
-​
-//         const daiTokenAddress = DAI_ADDRESS;
-//         assert(daiTokenAddress, 'DAI token address is undefined.');
-//         ​
-//         const daiToken = await ERC20.at(daiTokenAddress);
-//         assert(daiToken, 'DAI token is undefined.');
-//         ​
-//         const initialBalanceAssetToken = await daiToken.balanceOf(sender);
-//         console.log(`Initial Balance DAI Token: ${initialBalanceAssetToken.toString()}`);
-// ​
-//         const initialBalanceWhaleAssetToken = await daiToken.balanceOf(DAI_WHALE);
-//         console.log(`Initial Balance DAI Token for DAI WHALE: ${initialBalanceAssetToken.toString()}`);
+  try {
+    const network = processArgs.network();
+    console.log(`Script will be executed in network ${network}.`);
 
-//         // await daiToken.transfer()
-// ​
-// ​        const finalBalanceDaiToken = await daiToken.balanceOf(sender);
-//         console.log(`Final Balance DAI Token: ${finalBalanceDaiToken.toString()}`);
+    const accounts = await web3.eth.getAccounts();
+    assert(accounts, 'Accounts is undefined.');
+
+    const receiver = accounts[receiverIndex];
+    assert(receiver, 'Receiver is undefined.');
+
+    const amountWei = BigNumber(amount.toString()).times(10 ** 18);
+    // await receiver.transfer({ from: unlockedAccount, value: amountWei });
+    await web3.eth.sendTransaction({ from: unlockedAccount, to: receiver, value: amountWei });
+
+    //         const daiTokenAddress = DAI_ADDRESS;
+    //         assert(daiTokenAddress, 'DAI token address is undefined.');
+    //
+    //         const daiToken = await ERC20.at(daiTokenAddress);
+    //         assert(daiToken, 'DAI token is undefined.');
+    //
+    //         const initialBalanceAssetToken = await daiToken.balanceOf(sender);
+    //         console.log(`Initial Balance DAI Token: ${initialBalanceAssetToken.toString()}`);
+    //
+    //         const initialBalanceWhaleAssetToken = await daiToken.balanceOf(DAI_WHALE);
+    //         console.log(`Initial Balance DAI Token for DAI WHALE: ${initialBalanceAssetToken.toString()}`);
+
+    //         // await daiToken.transfer()
+    //
+    // ​        const finalBalanceDaiToken = await daiToken.balanceOf(sender);
+    //         console.log(`Final Balance DAI Token: ${finalBalanceDaiToken.toString()}`);
 
 
-        console.log('>>>> The script finished successfully. <<<<');
-        callback();
-    } catch (error) {
-        console.log(error);
-        callback(error);
-    }
-}
+    console.log('>>>> The script finished successfully. <<<<');
+    callback();
+  } catch (error) {
+    console.log(error);
+    callback(error);
+  }
+};
